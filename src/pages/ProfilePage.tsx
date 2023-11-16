de4f8816ba5744da3591a6588baeb4981be8c6ae
thangnt15 <imlight2001@gmail.com>
2023-11-16 18:30:19 +0700

    cmt1

---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,19 @@
+import '../App.css';
+
+const ProfilePage = () => {
+    return /*html*/(
+        <div className="profile">
+        <img
+          src="./public/z4089866810746_8630481cc291b4e10e266aa951a4f935.jpg"
+          alt="Profile Picture"
+        />
+      <h1>Nguyễn Thành Thắng</h1>
+      <p>Web Developer</p>
+      <p>23 years old</p>
+      <p>Ha Noi</p>
+      <p>Make MONEY</p>
+      <p>JS,PHP,ECMA</p>
+    </div>
+    )
+}
+export default ProfilePage;
